4fa6c8f0ad1a84afec9685e9e46bb2fd25b2f882
Deepak-sngh <96277019+Deepak-sngh@users.noreply.github.com>
2021-12-18 00:30:01 +0530

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,22 @@
+
+let temp =``;
+
+fetch("https://s3.amazonaws.com/open-to-cors/assignment.json").then((res) => {
+  res.json().then((data) => {
+    
+    if (Object.keys(data).length > 0) { 
+
+      Object.keys(data.products).forEach((val) => {
+    
+    
+        temp += `<tr> <td>  ${data.products[val].title} </td> <td>   ${data.products[val].subcategory}  </td> <td>   ${data.products[val].price}  </td> <td>   ${data.products[val].popularity}  </td></tr>`;
+
+      });
+ 
+
+      document.getElementById("myTable").innerHTML = temp;
+    }
+  });
+});
+
+
