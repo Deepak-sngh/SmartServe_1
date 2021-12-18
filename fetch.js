1a22ad873c110add07d75d74caab305fd623a92d
Deepak-sngh <96277019+Deepak-sngh@users.noreply.github.com>
2021-12-18 08:56:46 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -9,7 +9,12 @@ fetch("https://s3.amazonaws.com/open-to-cors/assignment.json").then((res) => {
       Object.keys(data.products).forEach((val) => {
     
     
-        temp += `<tr> <td>  ${data.products[val].title} </td> <td>   ${data.products[val].subcategory}  </td> <td>   ${data.products[val].price}  </td> <td>   ${data.products[val].popularity}  </td></tr>`;
+        temp += `<tr> <td>  ${data.products[val].title} </td>
+        <td>   ${data.products[val].subcategory}  </td>
+        <td>   ${data.products[val].price}  </td>
+        <td>   ${data.products[val].popularity}  </td>
+         <td>   ${data.count[val]}  </td>
+        </tr>`;
 
       });
  
